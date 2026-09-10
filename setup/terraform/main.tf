08082a849f3e8bf494ad0ead4db98b4e7c35baef
Aman Singh <am9720536083@gmail.com>
2026-09-10 19:00:28 +0530

    docs: add project submission screenshots

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -142,11 +142,11 @@ resource "aws_ecr_repository" "backend" {
 # Create an EKS cluster
 resource "aws_eks_cluster" "main" {
   name     = "cluster"
-  version  = var.k8s_version
+  version  = "1.30"
   role_arn = aws_iam_role.eks_cluster.arn
   vpc_config {
     subnet_ids              = [aws_subnet.private_subnet.id, aws_subnet.public_subnet.id]
-    endpoint_public_access  = var.enable_private == true ? false : true
+    endpoint_public_access  = true
     endpoint_private_access = true
   }
   depends_on = [aws_iam_role_policy_attachment.eks_cluster, aws_iam_role_policy_attachment.eks_service]
@@ -187,37 +187,29 @@ resource "aws_iam_role_policy_attachment" "eks_service" {
 # EKS Node Group
 ##################
 # Track latest release for the given k8s version
+/*
 data "aws_ssm_parameter" "eks_ami_release_version" {
   name = "/aws/service/eks/optimized-ami/${aws_eks_cluster.main.version}/amazon-linux-2/recommended/release_version"
 }
-
-resource "aws_eks_node_group" "main" {
-  node_group_name = "udacity"
+*/
+  resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
-  version         = aws_eks_cluster.main.version
+  node_group_name = "udacity"
   node_role_arn   = aws_iam_role.node_group.arn
-  subnet_ids      = [var.enable_private == true ? aws_subnet.private_subnet.id : aws_subnet.public_subnet.id]
-  release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value)
-  instance_types  = ["t3.small"]
+  subnet_ids      = [aws_subnet.public_subnet.id]
+  ami_type = "BOTTLEROCKET_x86_64"
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
+    desired_size = 2
+    max_size     = 3
     min_size     = 1
   }
 
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
     aws_iam_role_policy_attachment.node_group_policy,
     aws_iam_role_policy_attachment.cni_policy,
     aws_iam_role_policy_attachment.ecr_policy,
   ]
-
-  lifecycle {
-    ignore_changes = [scaling_config.0.desired_size]
-  }
 }
 
 // IAM Configuration
@@ -315,12 +307,12 @@ resource "aws_iam_role_policy_attachment" "codebuild" {
 resource "aws_iam_user" "github_action_user" {
   name = "github-action-user"
 }
-
+/*
 resource "aws_iam_user_policy" "github_action_user_permission" {
   user   = aws_iam_user.github_action_user.name
   policy = data.aws_iam_policy_document.github_policy.json
 }
-
+*/
 data "aws_iam_policy_document" "github_policy" {
   statement {
     effect    = "Allow"
